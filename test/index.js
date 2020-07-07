cdb4697d41dd03b730619ccc016a6aa5a2ddd1ae
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-07-06 22:50:55 -0300

    Testes de status code da do server-Erro

--- test/index.js
+++ test/index.js
@@ -1,13 +1,20 @@
-import axios from 'axios';
+import http from 'http';
 import assert from 'assert';
 
-// import server from '../lib/index.js';
+// describe('Node Server with ES6', () => {
+//     it('Status code return 200', (done) => {
+//         http.get(`http://localhost:${process.env.SERVER_PORT}`, (res) => { done(); })
+//         assert.equal(200, res.statusCode);
+//         done();
+//     });
+// });
 
-describe('Node Server with ES6', () => {
-    it('Status code return 200', done => {
-        const res = await axios.get(`http://localhost:${process.env.SERVER_PORT}`);
-        assert.equal(200, res.statusCode);
-        // server.close();
-        done();
-    });
+describe('Testing async', function(){
+	it('...get google page', function(done){
+		http.get(`http://localhost:${process.env.SERVER_PORT}`, function(res){
+			done();
+		}).on('error', function(e){
+			assert.fail(e);
+		})
+	});
 });
